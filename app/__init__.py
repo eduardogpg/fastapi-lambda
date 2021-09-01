cf8bd68414732ab948d8005d7c6bad8055a0ff74
eduardogpg <eduardo78d@gmail.com>
2021-09-01 11:45:00 -0500

    Mangum

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
+from mangum import Mangum
 
 app = FastAPI(debug=False)
 
 from .routers.v1 import router as routers_v1
 
 app.include_router(routers_v1)
+
+handler = Mangum(app)
