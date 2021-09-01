a6a3c80fe60cbfca1758a2650c80c6c70f63becc
eduardogpg <eduardo78d@gmail.com>
2021-09-01 11:54:38 -0500

    Prefix

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,13 @@
+import os
+from mangum import Mangum​
 from fastapi import FastAPI
-from mangum import Mangum
-
-app = FastAPI(debug=False)
-
+​
+stage = os.environ.get('STAGE', None)
+openapi_prefix = f"/{stage}" if stage else "/"
+​
+app = FastAPI(title="ms-stats", openapi_prefix=openapi_prefix) 
+​
 from .routers.v1 import router as routers_v1
 
 app.include_router(routers_v1)
-
 handler = Mangum(app)
